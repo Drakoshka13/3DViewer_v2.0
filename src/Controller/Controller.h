3e9cce43aa37124f7ed208758279f89b471b54c3
Mamie Malwyn <mamiemal@student.21-school.ru>
2023-10-22 17:11:05 +0300

    start

---
+++ src/Controller/Controller.h
@@ -0,0 +1,24 @@
+#ifndef CONSOLEVIEW_H
+#define CONSOLEVIEW_H
+
+#include "../Model/Model.h"
+// #include "../viewer_data.h"
+
+class Controller {
+ public:
+  static Controller& getInstance(Model* m) {
+    static Controller instance = Controller(m);
+    return instance;
+  }
+
+ private:
+  Controller() = delete;
+  Controller(Model* m) : model(m) {}
+  Model* model;
+
+  void SubMethods(coordinates& data);
+  void Rotation(coordinates& data);
+
+  void writing_data(info& data);
+  void read_file(info& data);
+};
